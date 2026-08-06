7a7124eb9f1c191f74d58c50d0ede3242f5316dd
qhathizwe <sphoswa2@gmail.com>
2026-08-06 14:57:16 +0200

    nav component to home page

--- src/Components/nav/Nav.tsx
+++ src/Components/nav/Nav.tsx
@@ -2,8 +2,19 @@ import styles from './Nav.module.css'
 import weatherIcon from '../../assets/nav and home-assets/weather-app.png'
 import savedLocations from '../../assets/nav and home-assets/saved.png'
 import settings from '../../assets/nav and home-assets/settings.png'
+import { useNavigate } from 'react-router-dom'
 
 export const Nav = () => {
+
+const navigate = useNavigate();
+const NavigateSettings =() =>{
+        navigate ("settings-page")
+}
+
+const Navigate = useNavigate();
+const NavigateSavedLocations = () =>{
+    Navigate ("saved-locations")
+}
   return (
     
     <div className={styles.navContainer}>
@@ -15,8 +26,8 @@ export const Nav = () => {
                 <p className={styles.slogan}>Know your weather, Plan your day</p>
             </div>
         <div className={styles.navIcons}>
-            <img src={savedLocations} alt='saved locations' className={styles.savedLocations}/>
-            <img src={settings} alt='settings' className={styles.settings}/>
+            <img src={savedLocations} alt='saved locations' className={styles.savedLocations} onClick={NavigateSavedLocations}/>
+            <img src={settings} alt='settings' className={styles.settings} onClick={NavigateSettings}/>
         </div>
     </div> 
   )
