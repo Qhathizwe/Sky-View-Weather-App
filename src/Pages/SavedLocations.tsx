502dd37141f056356833ae02bfef920214c86e08
qhathizwe <sphoswa2@gmail.com>
2026-08-06 15:55:32 +0200

    saved location component to a page

---
+++ src/Pages/SavedLocations.tsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom"
+
+const SavedLocations = () => {
+
+  return (
+    
+     
+    <div>SavedLocations
+        <button><NavLink to ='/'/>BACK</button>
+    </div>
+  )
+}
+export default SavedLocations
