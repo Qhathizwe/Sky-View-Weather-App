f0481f3ff2e8704b5bd45410ad714db5275c81d1
qhathizwe <sphoswa2@gmail.com>
2026-08-23 20:17:08 +0200

    not found content and style

---
+++ src/Pages/not found page/Not_Found.tsx
@@ -0,0 +1,19 @@
+// src/Pages/Not_Found.tsx
+import { useNavigate } from 'react-router-dom';
+import styles from './NotFound.module.css';
+
+const NotFound = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.code}>404</h1>
+      <p className={styles.message}>Oops! This page doesn't exist.</p>
+      <button className={styles.homeBtn} onClick={() => navigate('/')}>
+        Go Home
+      </button>
+    </div>
+  );
+};
+
+export default NotFound;
