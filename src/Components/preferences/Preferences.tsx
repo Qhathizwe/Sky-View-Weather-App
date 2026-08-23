c787e0d5c952a5cb3cd87732bc229cf6922cfb96
qhathizwe <sphoswa2@gmail.com>
2026-08-23 20:26:41 +0200

    adding conversions and passing tem to components

--- src/Components/preferences/Preferences.tsx
+++ src/Components/preferences/Preferences.tsx
@@ -1,22 +1,33 @@
-import styles from './Preferences.module.css'
-import PreferenceIcon from '../../assets/settings_Prefences/adjustment.png'
-import Themes from './Themes'
 
-const Preferences = () => {
+import styles from './Preferences.module.css';
+import PreferenceIcon from '../../assets/settings_Prefences/adjustment.png';
+import Themes from './Themes';
+import Units from '../preferences/Units';
+
+type PreferencesProps = {
+  units: '°C' | '°F';
+  changeUnits: () => void;
+};
+
+const Preferences: React.FC<PreferencesProps> = ({ units, changeUnits }) => {
   return (
     <div className={styles.PreferenceContainer}>
-        <div className={styles.PreferenceContent}> 
-          <div className={styles.title}>
-            <img src={PreferenceIcon} alt="Preference-Icon" />
-            <h1>Preferences</h1>
-          </div>
-
-          <div className={styles.theme}>
-            <Themes />
-          </div>
-          <div></div>
+      <div className={styles.PreferenceContent}>
+        <div className={styles.title}>
+          <img src={PreferenceIcon} alt="Preference-Icon" />
+          <h1>Preferences</h1>
         </div>
+
+        <div className={styles.theme}>
+          <Themes />
+        </div>
+        
+        <div className={styles.unitSection}>
+          <Units units={units} changeUnits={changeUnits} />
+        </div>
+      </div>
     </div>
-  )
-}
-export default Preferences
+  );
+};
+
+export default Preferences;
