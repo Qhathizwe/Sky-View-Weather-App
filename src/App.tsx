7b57727106ab68ed2261d0035a3a140d04774414
qhathizwe <sphoswa2@gmail.com>
2026-08-23 20:14:45 +0200

    routing not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import './App.css'
-import { Home } from './Pages/home/Home'
-import { Route, Routes } from 'react-router-dom'
-import SavedLocations from './Pages/SavedLocations'
-import SettingsPage from './Pages/Settings/SettingsPage'
-// import Not_Found from './Pages/Not_Found'
 
-
+import './App.css';
+import { Home } from './Pages/home/Home';
+import { Route, Routes } from 'react-router-dom';
+import SavedLocations from './Pages/Saved locations/SavedLocations';
+import SettingsPage from './Pages/Settings/SettingsPage';
+import NotFound from './Pages/not found page/Not_Found';
 
 function App() {
   return (
-    <>
     <Routes>
-    <Route path='/' element={<Home />}/>
-    <Route path='saved-locations' element={<SavedLocations />}/>
-    <Route path='settings-page' element={<SettingsPage />} />
-    {/* <Route path='page-not-found' element={Not_Found}/> */}
+      <Route path="/" element={<Home />} />
+      <Route path="saved-locations" element={<SavedLocations />} />
+      <Route path="settings-page" element={<SettingsPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
